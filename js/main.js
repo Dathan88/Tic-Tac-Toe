f96d6b7bf04ac801519fec666aa1192616a15beb
Dathan Spencer <dpspencer88@gmail.com>
2018-08-15 20:46:31 -0600

    Minor improvements

--- js/main.js
+++ js/main.js
@@ -1,52 +1,67 @@
-const game = () => {};
+"use strict";
+
+const gameBoard = (() => {
+	let gameBoardArray = ["", "", "","", "", "","", "", "",];
+
+	const clearGameBoard = () => {
+		this.gameBoardArray = ["", "", "","", "", "","", "", "",];
+	};
+
+	const makeBoard = (() => {
+
+	});
+});
+
+function board() {
+	let i;
+	const gameBoard = document.getElementById("gameBoard");
+
+	const boardLayout = document.createElement("TABLE");
+		boardLayout.classList.add("boardLayout");
+
+	gameBoard.appendChild(boardLayout);
 
 
-let btn = document.getElementById('btn');
-btn.addEventListener('click', gameBoard);
-
-function gameBoard() {
-	document.getElementById("btn").style.display = "none";
-	let container = document.querySelector("#container");
-		container.style.position = "absolute";
-		container.style.top = "0";
-		container.style.bottom = "0";
-		container.style.left = "0";
-		container.style.right = "0";
-		container.style.width = "40em";
-		container.style.height = "40em";
-		container.style.display = "grid";
-		container.style.margin = "auto";
-		container.style.gridTemplateColumns = "repeat(3, 32.5% [col-start])";
-		container.style.gridTemplateRows = "repeat(3, 32.5% [row-start])";
-		
-	for (i = 0; i < 9; i++) {
-		let squares = document.createElement('div');
+	for (i = 0; i < 3; i++) {
+		let squares = document.createElement('TR');
 			squares.classList.add('squares');
+
 			squares.addEventListener('click', (e) => {
-				if (squares.innerHTML === "X") {
-					squares.innerHTML = "O";
+				if (e.target.innerHTML === "X") {
+					e.target.innerHTML = "O";
 				} else {
-					squares.innerHTML = "X";
+					e.target.innerHTML = "X";
 				}
 			});
-		document.getElementById('container').appendChild(squares);
-	};
-};
-
-function Player(name, number) {
-	this.name = name;
-	this.number = number;
-	this.title = function() {
-		return this.name + " - " + "Player " + this.number;
+		boardLayout.appendChild(squares);
 	}
 }
 
-let bob = new Player("Bob", 1);
-let george = new Player("George", 2);
+function Player(name, number, symbol) {
+	this.name = name;
+	this.number = number;
+	this.symbol = symbol;
+	this.title = function() {
+		return this.name + " - " + "Player " + this.number + " Symbol = " + this.symbol;
+	};
+}
+
+let bob = new Player("Bob", 1, "X");
+let george = new Player("George", 2, "O");
 
 console.log(bob.title());
 console.log(george.title());
 
-// $(function() {
-// 	alert("Hey");
-// });
+document.getElementsByTagName("body").onload = board();
+
+
+/*$(function() {
+	alert("Hey");
+});*/
+
+/*var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello There!');
+}).listen(8080);*/
