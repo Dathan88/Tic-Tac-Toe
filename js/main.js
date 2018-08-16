233f5c0d42e872b10077a60263b4d5f8ea3fdbdd
Dathan Spencer <dpspencer88@gmail.com>
2018-08-16 14:22:54 -0600

    Work out layout

--- js/main.js
+++ js/main.js
@@ -1,67 +1,25 @@
 "use strict";
 
 const gameBoard = (() => {
-	let gameBoardArray = ["", "", "","", "", "","", "", "",];
+	let gameBoardArray = ["", "", "","", "", "","", "", ""];
+
 
 	const clearGameBoard = () => {
-		this.gameBoardArray = ["", "", "","", "", "","", "", "",];
+		this.gameBoardArray = ["", "", "","", "", "","", "", ""];
 	};
 
-	const makeBoard = (() => {
-
-	});
 });
 
-function board() {
-	let i;
-	const gameBoard = document.getElementById("gameBoard");
-
-	const boardLayout = document.createElement("TABLE");
-		boardLayout.classList.add("boardLayout");
-
-	gameBoard.appendChild(boardLayout);
-
-
-	for (i = 0; i < 3; i++) {
-		let squares = document.createElement('TR');
-			squares.classList.add('squares');
-
-			squares.addEventListener('click', (e) => {
-				if (e.target.innerHTML === "X") {
-					e.target.innerHTML = "O";
-				} else {
-					e.target.innerHTML = "X";
-				}
-			});
-		boardLayout.appendChild(squares);
-	}
+const personFactory = (name, position, symbol) => {
+	const info = () => { return name + " - " + "Player " + position + " Symbol = " + symbol };
+	return { name, position, info }
 }
 
-function Player(name, number, symbol) {
-	this.name = name;
-	this.number = number;
-	this.symbol = symbol;
-	this.title = function() {
-		return this.name + " - " + "Player " + this.number + " Symbol = " + this.symbol;
-	};
-}
+const playGame = () => {};
 
-let bob = new Player("Bob", 1, "X");
-let george = new Player("George", 2, "O");
-
-console.log(bob.title());
-console.log(george.title());
-
-document.getElementsByTagName("body").onload = board();
+let bob = personFactory("Bob", 1, "X");
+let george = personFactory("George", 2, "O");
 
 
-/*$(function() {
-	alert("Hey");
-});*/
-
-/*var http = require('http');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello There!');
-}).listen(8080);*/
+console.log(bob.name);
+console.log(george.info());
