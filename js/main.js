47fedd9cb47f97f51941974f9b273b28e1c2d0e1
Dathan Spencer <dpspencer88@gmail.com>
2018-08-16 19:28:57 -0600

    Work out JS a little more

--- js/main.js
+++ js/main.js
@@ -3,23 +3,56 @@
 const gameBoard = (() => {
 	let gameBoardArray = ["", "", "","", "", "","", "", ""];
 
-
 	const clearGameBoard = () => {
 		this.gameBoardArray = ["", "", "","", "", "","", "", ""];
 	};
 
+	const createGameBoard = () => {
+		$.each(gameBoardArray, function(index) {
+			let square = document.createElement("DIV");
+			$(square).attr({
+				"id" : "square" + (index + 1),
+				"class" : "squares"
+			});
+
+			$("div#board").append(square);
+		});
+
+		$(".squares").one("click", function() {
+			$(this).append("X");
+		});
+	};
+	createGameBoard();
 });
 
-const personFactory = (name, position, symbol) => {
+const playerFactory = (name, position, symbol) => {
 	const info = () => { return name + " - " + "Player " + position + " Symbol = " + symbol };
 	return { name, position, info }
 }
 
 const playGame = () => {};
 
-let bob = personFactory("Bob", 1, "X");
-let george = personFactory("George", 2, "O");
+let bob = playerFactory("Bob", 1, "X");
+let george = playerFactory("George", 2, "O");
 
-
-console.log(bob.name);
+gameBoard();
+console.log();
 console.log(george.info());
+
+
+/*gameBoardArray.forEach((item, index) => {
+	let squareId = gameBoardArray.indexOf(item) + 1;
+	let square = document.createElement("DIV");
+		square.setAttribute("class", "squares");
+		square.setAttribute("id", "square" + squareId);
+
+		square.addEventListener("click", (e) => {
+		if (square.innerHTML === "X") {
+			square.innerHTML = "O";
+		} else {
+			square.innerHTML = "X";
+		}
+	});
+	board.appendChild(square);
+	console.log(squareId);
+});*/
