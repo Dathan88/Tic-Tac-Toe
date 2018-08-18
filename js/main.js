b360663fce4a5c20cb6444e36e372bfe43bcbfad
Dathan Spencer <dpspencer88@gmail.com>
2018-08-17 19:42:48 -0600

    More css and js changes

--- js/main.js
+++ js/main.js
@@ -1,58 +1,57 @@
 "use strict";
 
 const gameBoard = (() => {
-	let gameBoardArray = ["", "", "","", "", "","", "", ""];
-
-	const clearGameBoard = () => {
-		this.gameBoardArray = ["", "", "","", "", "","", "", ""];
-	};
+	let gameBoardArray = ["X", "X", "O","X", "O", "O","O", "X", "O"];
 
 	const createGameBoard = () => {
 		$.each(gameBoardArray, function(index) {
-			let square = document.createElement("DIV");
+			const square = document.createElement("DIV");
 			$(square).attr({
 				"id" : "square" + (index + 1),
 				"class" : "squares"
 			});
-
 			$("div#board").append(square);
 		});
 
-		$(".squares").one("click", function() {
-			$(this).append("X");
-		});
+		const bottomDisplay = document.createElement("DIV");
+			$(bottomDisplay).attr("id", "bottomDisplay");
+			$("div#board").after(bottomDisplay);
+			$("#bottomDisplay").html(`<p id="player1Info"> ${player1.info()} </p>` + `<p id="player2Info"> ${player2.info()} </p>`);
+
+			$(".squares").one("click", function() {
+				$(this).text();
+			});
+	};
+
+	const clearGameBoard = () => {
+		this.gameBoardArray = ["", "", "","", "", "","", "", ""];
 	};
 	createGameBoard();
 });
 
-const playerFactory = (name, position, symbol) => {
-	const info = () => { return name + " - " + "Player " + position + " Symbol = " + symbol };
-	return { name, position, info }
+const playerFactory = (name, mark) => {
+	const getName = () => name;
+	const getMark = () => mark;
+	const move = () => {
+		$(".squares").one("click", function() {
+			$(this).append(name.mark);
+			return name + mark;
+		});
+	}
+	/*const win;
+	const lose;*/
+
+	const info = () => { return "Player : " + name + " = " + mark };
+	console.log();
+	return { name, mark, info };
 }
+
+let player1 = playerFactory("1", "X");
+let player2 = playerFactory("2", "O");
 
 const playGame = () => {};
 
-let bob = playerFactory("Bob", 1, "X");
-let george = playerFactory("George", 2, "O");
-
 gameBoard();
-console.log();
-console.log(george.info());
-
-
-/*gameBoardArray.forEach((item, index) => {
-	let squareId = gameBoardArray.indexOf(item) + 1;
-	let square = document.createElement("DIV");
-		square.setAttribute("class", "squares");
-		square.setAttribute("id", "square" + squareId);
-
-		square.addEventListener("click", (e) => {
-		if (square.innerHTML === "X") {
-			square.innerHTML = "O";
-		} else {
-			square.innerHTML = "X";
-		}
-	});
-	board.appendChild(square);
-	console.log(squareId);
-});*/
+console.log(playerFactory.name);
+console.log(player1);
+console.log(player2.info());
