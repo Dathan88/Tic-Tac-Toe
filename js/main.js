da3b4260c9f6d8d963bf72f7832115b78a29fe78
Dathan Spencer <dpspencer88@gmail.com>
2018-09-18 17:46:27 -0600

    Get game and win/tie logic working

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 'use strict';
-//Helps with gamePlay function
+//'let turn' helps with playerTurn function - tracks player's turn
 let turn = -1;
 
 //Prompts for user input on player name/mark
@@ -20,21 +20,32 @@ const userInput = (() => {
 //factory to take the user input to create players
 const Player = (name, mark) => {
 	(this.name = name), (this.mark = mark);
+	const moves = [];
 	const info = () => {
 		return name + ' = ' + mark;
 	};
-	return { name, mark, info };
+	return { name, mark, moves, info };
 };
 
 //Players info from userinput object and sends them to Player factory
 const player1 = Player(userInput.user1, userInput.mark1);
 const player2 = Player(userInput.user2, userInput.mark2);
-let player1Array = [];
-let player2Array = [];
 
-//Object housing game board array for gameboard moves
+//Object containing boardArray for gameboard display/moves
 const gameBoard = {
 	boardArray: ['', '', '', '', '', '', '', '', ''],
+};
+
+//Object containing arrays of winning moves
+const winningMoves = {
+	play1: [0, 1, 2],
+	play2: [3, 4, 5],
+	play3: [6, 7, 8],
+	play4: [0, 3, 6],
+	play5: [1, 4, 7],
+	play6: [2, 5, 8],
+	play7: [0, 4, 8],
+	play8: [2, 4, 6],
 };
 
 //Creates/Displays board
@@ -57,13 +68,17 @@ function renderBoard() {
 
 		return index;
 	});
-	//Calls gamePlay function
-	gamePlay();
+	//Calls playerTurn function
+	playerTurn();
 }
 
 //create players display
 const createPlayerDisplays = (function() {
 	const playerDisplay = document.createElement('DIV');
+
+	$('#topDisplay').append(function() {
+		$('#topDisplay').html(`<p >~ ${player1.name}'s Turn ~ </p>`);
+	});
 	$(playerDisplay).attr('id', 'playerDisplay');
 	$('div#board').after(playerDisplay);
 	$('#playerDisplay').html(
@@ -77,78 +92,77 @@ function clearBoard() {
 	turn = -1;
 	$('.squares').remove();
 	$('#topDisplay').empty();
-	player1Array = [];
-	player2Array = [];
+	player1.array = [];
+	player2.array = [];
 	gameBoard.boardArray = ['', '', '', '', '', '', '', '', ''];
 	renderBoard();
-	return turn, player1Array, player2Array;
+	return turn, player1.array, player2.array;
 }
 
 //Controls players turn and display above board
-function gamePlay() {
-	$('#topDisplay').append(function() {
-		$('#topDisplay').html(`<p >~ ${player1.name}'s Turn ~ </p>`);
-	});
-
+function playerTurn() {
 	$('p.squareContent').on('click', function() {
 		const squareIndex = $('p.squareContent').index(this);
 		turn++;
 		if (turn % 2 === 0) {
 			$(this).text(player1.mark);
-			player1Array.push(squareIndex);
+			player1.moves.push(squareIndex);
 			gameBoard.boardArray.splice(squareIndex, 1, player1.mark);
 			$('#topDisplay').html(`<p>~ ${player2.name}'s Turn ~</p>`);
 		} else {
 			$(this).text(player2.mark);
-			player2Array.push(squareIndex);
+			player2.moves.push(squareIndex);
 			gameBoard.boardArray.splice(squareIndex, 1, player2.mark);
 			$('#topDisplay').html(`<p>~ ${player1.name}'s Turn ~</p>`);
 		}
 		$(this).off('click');
 
-		if (turn >= 4) {
-			rules();
+		if (turn === 8) {
+			$('#topDisplay').html(`<p> Draw. Game Over. </p>`);
+			checkWinner();
+		} else if (turn === 7) {
+			$('#topDisplay').html(`<p> Last Move. </p>`);
+			checkWinner();
+		} else if (turn > 3) {
+			checkWinner();
 		}
-		// console.log(turn);
-		console.log(player1Array, player2Array);
+
+		console.log(turn);
 	});
-	return player1Array, player2Array;
+	return player1.moves, player2.moves;
 }
 
-function rules() {
-	const winningPlays = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[0, 4, 8],
-		[2, 4, 6],
-	];
+function checkWinner() {
+	let player1Count = 0;
+	let player2Count = 0;
+	const values = Object.values(winningMoves);
 
-	function winner(play, player) {
-		return play === player;
-	}
-
-	for (let x of winningPlays) {
-		let result = x.map(function(y) {
-			return parseInt(y, 10);
-		});
-
-		let winningMove = x.sort(function(a, b) {
-			return a - b;
-		});
-
-		let winPlayer = player1Array.sort(function(a, b) {
-			return a - b;
-		});
-
-		console.log(winningMove, winPlayer, result);
-		winner(winningMove, winPlayer);
+	for (const value of values) {
+		player1Count = 0;
+		player2Count = 0;
+		//console.log(player1.moves);
+		for (let i = 0; i < value.length; i++) {
+			if (player1.moves.includes(value[i])) {
+				player1Count++;
+				//console.log(player1Count);
+				if (player1Count === 3) {
+					$('#topDisplay').html(
+						`<p> ${player1.name} Wins!!! Game Over!!! </p>`
+					);
+					$('p.squareContent').off('click');
+				}
+			} else if (player2.moves.includes(value[i])) {
+				player2Count++;
+				if (player2Count === 3) {
+					console.log('player 2 Winner!');
+					$('#topDisplay').html(
+						`<p> ${player2.name} Wins!!! Game Over!!! </p>`
+					);
+					$('p.squareContent').off('click');
+				}
+			}
+		}
 	}
 }
 
 renderBoard();
-
-//parseInt(winPlayer.join('')) === parseInt(winElement.join(''))
